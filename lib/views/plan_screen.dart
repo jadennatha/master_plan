4f7583cbae278ebac07999d5960b8e58c0643e37
jadennatha <144076853+jadennatha@users.noreply.github.com>
2025-11-04 17:41:08 +0700

    Initial commit

---
+++ lib/views/plan_screen.dart
@@ -0,0 +1,112 @@
+import '../models/data_layer.dart';
+import 'package:flutter/material.dart';
+
+class PlanScreen extends StatefulWidget {
+  const PlanScreen({super.key});
+
+  @override
+  State createState() => _PlanScreenState();
+}
+
+class _PlanScreenState extends State<PlanScreen> {
+  Plan plan = const Plan();
+  late ScrollController scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController = ScrollController()
+      ..addListener(() {
+        FocusScope.of(context).requestFocus(FocusNode());
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    // ganti ‘Namaku' dengan Nama panggilan Anda
+    appBar: 
+      AppBar(
+        title: const Text('Master Plan Jaden'),
+        backgroundColor: Color.fromARGB(230, 154, 21, 216),
+        titleTextStyle: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    body: _buildList(),
+    floatingActionButton: _buildAddTaskButton(),
+   );
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildAddTaskButton() {
+    return FloatingActionButton(
+    backgroundColor: Color.fromARGB(230, 154, 21, 216),
+    shape: CircleBorder(), 
+      onPressed: () {
+        setState(() {
+          plan = Plan(
+            name: plan.name,
+            tasks: List<Task>.from(plan.tasks)
+            ..add(const Task()),
+          );
+        });
+      },
+    child: const Icon(Icons.add, color: Colors.white),
+    );
+  }
+
+  Widget _buildList() {
+    return ListView.builder(
+      itemCount: plan.tasks.length,
+      itemBuilder: (context, index) =>
+      _buildTaskTile(plan.tasks[index], index),
+      controller: scrollController,
+      keyboardDismissBehavior: Theme.of(context).platform ==
+      TargetPlatform.iOS
+                ? ScrollViewKeyboardDismissBehavior.onDrag
+                : ScrollViewKeyboardDismissBehavior.manual,
+    );
+  }
+
+  Widget _buildTaskTile(Task task, int index) {
+    return ListTile(
+      leading: Checkbox(
+          value: task.complete,
+          onChanged: (selected) {
+            setState(() {
+              plan = Plan(
+                name: plan.name,
+                tasks: List<Task>.from(plan.tasks)
+                  ..[index] = Task(
+                    description: task.description,
+                    complete: selected ?? false,
+                  ),
+              );
+            });
+          }),
+      title: TextFormField(
+        initialValue: task.description,
+        onChanged: (text) {
+          setState(() {
+            plan = Plan(
+              name: plan.name,
+              tasks: List<Task>.from(plan.tasks)
+                ..[index] = Task(
+                  description: text,
+                  complete: task.complete,
+                ),
+            );
+          });
+        },
+      ),
+    );
+  }
+}
